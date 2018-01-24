e87da3d9b025da07bd66b0870d220e745263162f
Julia Fovaeus <fovaeus@gmail.com>
2018-01-25 00:33:51 +0100

    A bit of clean up. Alternative instrumental distribution function.

--- inverseMethod.m
+++ inverseMethod.m
@@ -1,8 +1,8 @@
 function f_sample = inverseMethod(Finv, sampleSize)
-    f_sample = zeros(sampleSize,1)
+    f_sample = zeros(sampleSize,1);
     for i = 1:sampleSize
-        u = rand(1)
-        f_sample(i) = Finv(u)
+        %u = rand(1);
+        f_sample(i) = Finv(rand(1));
     end
 end
         
