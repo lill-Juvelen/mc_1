3f5a04ec6cea38a57f4cb64f2a2a308b0d553a83
Julia Fovaeus <fovaeus@gmail.com>
2018-01-24 10:06:08 +0100

    computer session 1

---
+++ inverseMethod.m
@@ -0,0 +1,8 @@
+function f_sample = inverseMethod(Finv, sampleSize)
+    f_sample = zeros(sampleSize,1)
+    for i = 1:sampleSize
+        u = rand(1)
+        f_sample(i) = Finv(u)
+    end
+end
+        
